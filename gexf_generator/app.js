4255ce0eeccca5b0322d8e18acc9f74d32afb4a0
michaelzcom <michael.zhang@zcomsystems.com>
2016-12-13 12:51:39 -0500

    dependency update
    linked dependency to the graph data structure

--- gexf_generator/app.js
+++ gexf_generator/app.js
@@ -1,5 +1,9 @@
 var gen = require('./gexfgen.js');
+var digraph = require('../GraphDataStructure/digraph');
+var attr = require('../GraphDataStructure/attribute');
 
-var a = "";
+var graph = digraph();
+graph.add.node(1,'hi',10,10,10,null,null,null);
+console.log(graph.graph);
 
-gen.graphGenerator(a);
+//gen.graphGenerator(g);
