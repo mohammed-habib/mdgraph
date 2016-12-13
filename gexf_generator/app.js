6094064c5156f467c5e110175232f78107aee40c
michaelzcom <michael.zhang@zcomsystems.com>
2016-12-13 15:24:51 -0500

    Added require colour module from graph DS
    Implemented use of colour in the form of r, g, b

--- gexf_generator/app.js
+++ gexf_generator/app.js
@@ -1,9 +1,17 @@
-var gen = require('./gexfgen.js');
+var generator = require('./gexfgen.js');
 var digraph = require('../GraphDataStructure/digraph');
-var attr = require('../GraphDataStructure/attribute');
+var attribute = require('../GraphDataStructure/attribute');
+var colour = require('../GraphDataStructure/colour');
 
 var graph = digraph();
-graph.add.node(1,'hi',10,10,10,null,null,null);
-console.log(graph.graph);
+graph.add.node(1,'a',50,50,1,new colour(0,0,255), 'GroupA',new attribute('key', 'value'));
+graph.add.node(2,'b',50,50,1,new colour(255,0,0), 'GroupB',new attribute('key', 'value'));
+graph.add.node(3,'c',50,50,1,new colour(0,255,0), 'GroupB',new attribute('key', 'value'));
+graph.add.edge(100,'a','b', 'asd');
+graph.add.edge(101,'a','c','LABEL');
+var g = graph.export.obj();
+console.log(g);
+console.log(g.nodes[0].getAttr());
+console.log(g.nodes[0].getColour());
 
-//gen.graphGenerator(g);
+generator.generate(g);
