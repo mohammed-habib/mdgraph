f12a7b848ce1296d7aefccea20737895313d53d9
michaelzcom <michael.zhang@zcomsystems.com>
2016-12-14 12:11:21 -0500

    Update to node generator structure
    Added colour and attributes to node creation
    Modularized metadata containing gexf output description
    Changed example nodes in app.js to test changes

--- gexf_generator/app.js
+++ gexf_generator/app.js
@@ -4,14 +4,11 @@ var attribute = require('../GraphDataStructure/attribute');
 var colour = require('../GraphDataStructure/colour');
 
 var graph = digraph();
-graph.add.node(1,'a',50,50,1,new colour(0,0,255), 'GroupA',new attribute('key', 'value'));
-graph.add.node(2,'b',50,50,1,new colour(255,0,0), 'GroupB',new attribute('key', 'value'));
-graph.add.node(3,'c',50,50,1,new colour(0,255,0), 'GroupB',new attribute('key', 'value'));
+graph.add.node(1,'a',0,0,10,new colour(0,0,255), 'GroupA',new attribute('acategory', 'valueA'));
+graph.add.node(2,'b',25,25,10,new colour(255,0,0), 'GroupB',new attribute('acategory', 'valueB'));
+graph.add.node(3,'c',50,50,10,new colour(0,255,0), 'GroupB',new attribute('acategory', 'valueC'));
 graph.add.edge(100,'a','b', 'asd');
 graph.add.edge(101,'a','c','LABEL');
 var g = graph.export.obj();
-console.log(g);
-console.log(g.nodes[0].getAttr());
-console.log(g.nodes[0].getColour());
 
 generator.generate(g);
