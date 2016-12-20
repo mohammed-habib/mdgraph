7449d17d865d9eabb1751975e89c02936f1f4dd0
Michael Zhang <michael.zhang@zcomsystems.com>
2016-12-20 12:26:54 -0500

    Removed unnecessary files

--- gexf_generator/metadata.js
+++
@@ -1,7 +0,0 @@
-// Metadata variables
-var metadata = {
-	creator : 'Michael Zhang',
-	date : 'yyyy-mm-dd',
-	description : 'File creation based on Gephi 0.9 gexf file and SigmaJS example gexf file for filters'
-};
-module.exports = metadata;
