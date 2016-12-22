26ab6cb608608a843f5112f72f045f825f6c5055
rutpatel <rutpatel010@gmail.com>
2016-12-22 15:49:14 -0500

    Update parsexml.js

--- xmlParseCode/parsexml.js
+++ xmlParseCode/parsexml.js
@@ -5,22 +5,22 @@ var DOMParser = require('xmldom').DOMParser;
 var os = require("os");
 var mkdirp = require('mkdirp');
 var itemMap = require('./itemMap.js');
+var path = require('path');
 
 //asking for the directory and the file name of the .xml file to parse
 //
 var parseWhat = readlineSync.question('Parse only one file or the whole folder containing xml files? (file / folder): ');
 var dir = readlineSync.question('Enter the path to the file directory: ');
 
-// container is the location to write all the folders/files to
-var container = dir+'/allScriptFiles';
+
 
 if(parseWhat == 'file'){
-    var container = dir+'/allScriptFiles';
+    var container = path.join(dir, 'allScriptFiles');
     var file = readlineSync.question('Enter the File Name: ');
     readOneFile(dir, file, container);
  }
 else if(parseWhat == 'folder'){ 
-    var container = dir+'/allScriptFiles';
+    var container = path.join(dir, 'allScriptFiles');
     readFolder(dir, container);
 }
 
@@ -28,23 +28,25 @@ var fileName; //stores fileName. e.g. cmap_ItemAction
 
 // readFolder(dir) reads all the xml files that currently exists inside the folder and parse the metadat 
 //of each file and make their respective sql and js folder containing .js and .sql files
+
 function readFolder(dir, container){
-  mkdirp(container, function(err){
-   if(err){
-     console.log('Error Message: ' +err);
+  
+    if(!(fs.existsSync(container))){
+        fs.mkdirSync(container);
     }
-  }); 
-  fs.readdir(dir, (err, files) => {
-     files.forEach(file => {
-       var fileLen = file.length; 
-       var suffix = file.substr(fileLen-4, fileLen-1); 
-       if(suffix == '.xml'){ // checks for xml files 
-           readOneFile(dir, file, container);
-       }
-       else if((fs.lstatSync(dir+'/'+file).isDirectory()) && (file != 'allScriptFiles')) {
-           readFolder(dir+'/'+file, container+'/'+file); 
-       } 
-     });
+
+    fs.readdir(dir, (err, files) => {
+  
+      files.forEach(function(file) {
+          var fileLen = file.length; 
+          var suffix = file.substr(fileLen-4, fileLen-1); 
+          if(suffix == '.xml'){ // checks for xml files 
+            return readOneFile(dir, file, container);
+          }
+          else if((fs.lstatSync(path.join(dir, file)).isDirectory()) && (file != 'allScriptFiles')) {
+             return readFolder(path.join(dir, file) , path.join(container, file)); 
+          } 
+      });
    }) 
 }
 
@@ -53,69 +55,84 @@ function readOneFile(dir, file, container){
    //fileLocation is the exact location of the xml file
    var fileLocation = getFileLocation(dir, file);
    startParsing(fileLocation, container);
+
 }
 
 //getFileLocation returns the exact fileLocation. C://../../cmap_Iac.xml
 function getFileLocation(dir, file){
-   var fileLen = file.length;
-   //xmlCheck is used to check if the last 4 characters of the file input are '.xml' or not
-   var xmlCheck = file.substr(fileLen-4, fileLen-1);
-  if(xmlCheck === '.xml'){   //if the user typed in the file name with .xml already attached at the back
+    
+    var fileLen = file.length;
+    //xmlCheck is used to check if the last 4 characters of the file input are '.xml' or not
+    var xmlCheck = file.substr(fileLen-4, fileLen-1);
+    
+    if(xmlCheck === '.xml'){   //if the user typed in the file name with .xml already attached at the back
       fileName = file.substring(0, fileLen-4);
-      return dir + '/'+file;
+      return path.join(dir, file);
     }
+    
     else {
       fileName=file;
-      return dir + '/' + file + '.xml';
+      return path.join(dir, file+'.xml');
     } 
 }
 
 // startParsing reads the metaData file and calls makeFiles if there exists a method tag
 // or makeFilesWithoutMethod is there are no methos tags but only script tags
 function startParsing(fileLocation, container){
-    if(fileExists(fileLocation)){
-        var xmldoc = fs.readFileSync(fileLocation, 'utf8');
-        var doc = new DOMParser().parseFromString(xmldoc, 'text/xml');
-        console.log(doc);
+    
+    if(fileExists(fileLocation)){   
+    
+        var doc = new DOMParser().parseFromString(fs.readFileSync(fileLocation, 'utf8'), 'text/xml');
         var methods = doc.getElementsByTagName('method'); 
         var scrDataVal = doc.getElementsByTagName('script');
         var itemMapVal = doc.getElementsByTagName('itemMap');
         var scrLength = scrDataVal.length;
+        
         if(scrLength != 0){
           var cntName = container+'/File-'+fileName;
-          mkdirp(cntName);
-           var scrName = scrDataVal[0].getAttribute('name');
-           scrName = scrName.trim(); //removes empty spaces from scrName 
-           if(itemMapVal.length !== 0){ // there are itemMap tags 
+          
+          if(!(fs.existsSync(cntName))){
+            fs.mkdirSync(cntName);
+          }
+          
+          var scrName = scrDataVal[0].getAttribute('name');
+          scrName = scrName.trim(); //removes empty spaces from scrName 
+          
+          if(itemMapVal.length !== 0){ // there are itemMap tags 
                 itemMap.readItemMap(cntName, itemMapVal, fileName, scrDataVal);
                 //makeFiles(container, methods);
-            }
-            else if (methods.length !== 0){ //there are method tags
+          }
+          else if (methods.length !== 0){ //there are method tags
               makeFiles(cntName, methods);
-            }
-            else { // no method tags but script tags
-               if(scrName.length != 0){ //if name exists, there can be a script tag within this script tag. i.e. metadata files with prefix 'script_'
-                   makeFiles(cntName, scrDataVal);
+          }
+          else { // no method tags but script tags
+              
+              if(scrName.length != 0){ //if name exists, there can be a script tag within this script tag. i.e. metadata files with prefix 'script_'
+                 makeFiles(cntName, scrDataVal);
+              }
+              else{
+                 makeFilesWithoutMethod(cntName, scrDataVal);
                }
-               else{
-                   makeFilesWithoutMethod(cntName, scrDataVal);
-               }
-         }
-       }
-     }
+          }
+        }   
+      }
+     
      else { //file location doesn't exist
        console.log('File not found at '+fileLocation);
     }
+
 }
 
 //nameExists returns true if the name exists in nameArr, false otherwise
 function nameExists(nameArr, name){
-   var lengthArr = nameArr.length; 
-   for(var j =0 ; j < lengthArr; j++){
-    if(nameArr[j] == name){
-      return true;
+    var lengthArr = nameArr.length; 
+    
+    for(var j =0 ; j < lengthArr; j++){
+        if(nameArr[j] == name){
+            return true;
+        }
     }
-   }
+   
    return false;
 }
 
@@ -124,6 +141,7 @@ function nameExists(nameArr, name){
 function makeFiles(dir, methods){
   var length = methods.length; 
   var nameArr = [];
+  
   for(var i = 0; i < length; ++i){ 
       var scriptDataValue = methods[i].getElementsByTagName('script');
       if(scriptDataValue.length == 0){
@@ -131,91 +149,80 @@ function makeFiles(dir, methods){
       } 
       var name = methods[i].getAttribute('name');
       name = name.replace('.', "_"); 
+      
       if(nameExists(nameArr, name)){ //if nameExists(arr, name) returns true
           name = name+i;
       }
+      
       nameArr.push(name);
       var paramList = methods[i].getElementsByTagName('parameter');
       var sqlDir = dir+'/sqlFiles-'+fileName;
+      
       if((name === 'cwOnFinderSQLGet') || ( name === 'cwOnFinderSQLSel')){ //create .sql File
-         if(!(fileExists(sqlDir))){
-              mkdirp(sqlDir);
-         }
-         var sqlData = methods[i].getElementsByTagName('script')[0].firstChild.nodeValue;
-         fs.writeFile(sqlDir+'/'+name+'.sql', sqlData, function(err){
-              if(err){
-                console.log('Error Message: ' + err);
-              }
-              else{
-                console.log('sql File Saved.');
-                }
-          })
-       }
+      
+         if(!(fs.existsSync(sqlDir))){
+              fs.mkdirSync(sqlDir);
+              var sqlData = methods[i].getElementsByTagName('script')[0].firstChild.nodeValue;
+              var reqPath = path.join(sqlDir, name+'.sql');
+               fs.writeFileSync(reqPath, sqlData, 'utf8');
+          }
+      
+      }  
+      
       else{ //create .js file
          var jsDir = dir+'/jsFiles-'+fileName;
-         mkdirp(jsDir, function(err){
-            if(err){
-              console.log('Error Message: ' +err);
-            }
-         });
+         
+         if(!(fs.existsSync(jsDir))){
+            fs.mkdirSync(jsDir);
+         }
+         
          var scriptVal = methods[i].getElementsByTagName('script')[0].firstChild.nodeValue; //script value within the script tag
          //Parameters...
-         if(paramList.length === 0){ //there are no parameters 
+          
+          if(paramList.length === 0){ //there are no parameters 
             var noParamJsData = 'function ' +name+ '(){ '+os.EOL +'   '+scriptVal +os.EOL+  '}';
-            fs.writeFile(jsDir+'/'+name+'.js', noParamJsData, function(err) {
-                   if (err) {  
-                       console.log("Error message: " + err);
-                   } else {
-                       console.log('js File Saved.');
-                   }
-             })
+            fs.writeFileSync(path.join(jsDir, name+'.js'), noParamJsData, 'utf8');
           } 
+          
           else { //there are parameters 
               var plength = paramList.length;
               var jsData = 'function ' +name+'(' ;
+              
               for(var k = 0; k < plength; ++k){
+                  
                   if(k === plength-1){
                       jsData += paramList[k].getAttribute('name');
                   }
+                  
                   else{
                       jsData += paramList[k].getAttribute('name') + ', ';
                   }
               }
+              
               jsData += '){'+os.EOL +'    ' +scriptVal +os.EOL+ '}';   
-              fs.writeFile(jsDir+'/'+name+'.js', jsData, function(err){
-               if(err){
-                  console.log('Error Message: ' + err);
-               }
-                 else{
-                  console.log('js File Saved.');
-               }
-              })
+              fs.writeFileSync(jsDir+'/'+name+'.js', jsData, 'utf8');
           }
       } 
   }
 }
 
 function makeFilesWithoutMethod(dir, scriptData){ //when there are no <method> tags but only <script> tags in metadata
-  var scriptDir = dir+'/jsFiles-'+fileName;
-  var length = scriptData.length;
-  mkdirp(scriptDir, function(err){
-          if(err){
-              console.log('Error Message: ' +err);
-          }
-    });
-   for(var i = 0; i < length; i++){
+    var scriptDir = dir+'/jsFiles-'+fileName;
+    var length = scriptData.length;
+  
+    if(!(fs.existsSync(scriptDir))){
+     fs.mkdirSync(scriptDir);
+    }  
+   
+    for(var i = 0; i < length; i++){
+    
         var dataScr = scriptData[i].firstChild.nodeValue;
         dataScr = dataScr.trim();
+        
         if(dataScr.length != 0){
            var dataVal = 'function script() {' + os.EOL + '   ' +dataScr + os.EOL+ '}';
-           fs.writeFile(scriptDir+'/script'+[i]+'.js', dataVal, function(err){
-                  if(err){
-                    console.log('Error Message: ' + err);
-                  }
-                  else{
-                    console.log('js File Saved.');
-                  }
-           })
+           fs.writeFileSync(scriptDir+'/script'+[i]+'.js', dataVal, 'utf8');
          }
+    
     }
 }
