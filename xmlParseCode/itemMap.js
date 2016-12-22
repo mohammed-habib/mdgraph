595a1debeb26b739fc08d60263e72d81051c47dd
rutpatel <rutpatel010@gmail.com>
2016-12-22 16:14:05 -0500

    Update itemMap.js

--- xmlParseCode/itemMap.js
+++ xmlParseCode/itemMap.js
@@ -3,7 +3,7 @@ var fileExists = require('file-exists');
 var readlineSync = require('readline-sync');
 var DOMParser = require('xmldom').DOMParser;
 var os = require("os");
-var mkdirp = require('mkdirp');
+var path = require('path');
 
 
 function readItemMap(container, itemMapVal, fileName, scrDataVal){ 
@@ -11,20 +11,22 @@ function readItemMap(container, itemMapVal, fileName, scrDataVal){
   var itemLocation = container+'/itemMap-'+fileName;
   var scriptsInItemFile = scrDataVal;
   if(scriptsInItemFile.length != 0){ //only make directory if script tag exists inside the metaData
-      mkdirp(itemLocation);
+      if(!(fs.existsSync(itemLocation))){
+          fs.mkdirSync(itemLocation);
+      }
       for(var k = 0; k < itemLength; k++){
           var itemName = itemMapVal[k].getAttribute('name');
           var methods = itemMapVal[k].getElementsByTagName('method');
           var scriptDataInItem = itemMapVal[k].getElementsByTagName('script');
           if(methods.length != 0){
                var specificItemDir = itemLocation+'/'+itemName;
-               mkdirp(specificItemDir);
+               fs.mkdirSync(specificItemDir);
                var itemMethodName = itemName+'_';
                makeMethodFiles(specificItemDir, methods, itemMethodName);
            }
            else if(scriptDataInItem.length != 0){
               // no method tags but script tags
-              mkdirp(specificItemDir);
+              fs.mkdirSync(specificItemDir);
               makeFilesWoMethod(specificItemDir, scriptDataInItem, itemName);
            }
            else{
@@ -47,49 +49,34 @@ function makeMethodFiles(itemDir, methods, itemFileName){
     var paramList = methods[i].getElementsByTagName('parameter');
      if((name === 'cwOnFinderSQLGet') || ( name === 'cwOnFinderSQLSel')){ //create .sql File
               var sqlData = methods[i].getElementsByTagName('script')[0].firstChild.nodeValue;
-              fs.writeFile(itemDir+'/'+name+'.sql', sqlData, function(err){
-                    if(err){
-                    console.log('Error Message: ' + err);
-                  }
-                  else{
-                    console.log('sql File Saved.');
-                    }
-               })
+              fs.writeFileSync(itemDir+'/'+name+'.sql', sqlData);
+              console.log('sql file saved.');
       }
       else{ //create .js file
             var scriptVal = methods[i].getElementsByTagName('script')[0].firstChild.nodeValue;
             //Parameters...
+            
             if(paramList.length === 0){ //there are no parameters 
               var jsDataWoParam = 'function ' +methodName+ '(){ '+os.EOL +'   '+scriptVal +os.EOL+ '}';
-              fs.writeFile(itemDir+'/'+name+'.js', jsDataWoParam, function(err) {
-                       if (err) {  
-                          console.log("Error message: " + err);
-                     } else {
-                          console.log('js File Saved.');
-                     }
-                })
+              fs.writeFileSync(path.join(itemDir, name+'.js'), jsDataWoParam);
+              console.log('js file saved.')
             } 
-           else { //there are parameters 
-            console.log(itemFileName);
-             var plength = paramList.length;
-             var jsData = 'function ' +methodName+'(' ;
-             for(var k = 0; k < plength; ++k){
-                if(k === plength-1){
-                  jsData += paramList[k].getAttribute('name');
+            else { //there are parameters 
+                //console.log(itemFileName);
+                var plength = paramList.length;
+                var jsData = 'function ' +methodName+'(' ;
+                for(var k = 0; k < plength; ++k){
+                    if(k === plength-1){
+                      jsData += paramList[k].getAttribute('name');
+                    }
+                    else{
+                      jsData += paramList[k].getAttribute('name') + ', ';
+                    }
                 }
-                else{
-                  jsData += paramList[k].getAttribute('name') + ', ';
-                }
-              }
+                
                 jsData += '){'+os.EOL +'    ' +scriptVal+os.EOL +'}';   
-                fs.writeFile(itemDir+'/'+name+'.js', jsData, function(err){
-                  if(err){
-                    console.log('Error Message: ' + err);
-                  }
-                  else{
-                    console.log('js File Saved.');
-                  }
-                 })
+                fs.writeFileSync(path.join(itemDir, name)+'.js', jsData);
+                console.log('js file saved.');
              }
          } 
        }
@@ -102,14 +89,8 @@ function makeFilesWoMethod(dir, scriptData, itemName){ //when there are no <meth
        dataScr = dataScr.trim();
        if(dataScr.length != 0){
          var dataVal = 'function (){ '+ os.EOL + '   ' +dataScr+os.EOL;
-         fs.writeFile(dir+itemName+'_script'+[i]+'.js', dataVal, function(err){
-                if(err){
-                  console.log('Error Message: ' + err);
-                }
-                else{
-                  console.log('js File Saved.');
-                }
-          })
+         fs.writeFileSync(dir+itemName+'_script'+[i]+'.js', dataVal);
+         console.log('js file saved.');
        }
    }
 }
